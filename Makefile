41f7817fb8eb331b083579033e518e3696ef86a0
Reza <reza.abasalipour@snapp.cab>
2023-07-30 00:00:12 +0330

    feat:local github-runner with corresponding installer

---
+++ Makefile
@@ -0,0 +1,9 @@
+.ONESHELL:
+.DEFAULT_GOAL := help
+.PHONY: help
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+install-runner:
+	@./scripts/install-github-runner.sh
---
+++ scripts/install-github-runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! helm version &> /dev/null
+then
+    echo "Helm not found, installing helm..."
+    
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+fi
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.3/cert-manager.crds.yaml
+
+helm upgrade --install --create-namespace -f ./templates/github-runner/values.yaml \
+    github-runner ./templates/github-runner --set github_token=${GITHUB_TOKEN} \
+    --wait --namespace actions-runner-system --create-namespace
+
+
