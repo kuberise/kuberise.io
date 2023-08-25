19e28448f9992ae9adfebcacc043b28022138b92
rapour <reza.apour@gmail.com>
2023-08-25 18:13:38 +0330

    fix:install script

--- cicd/scripts/install-github-runner.sh
+++ cicd/scripts/install-github-runner.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+
+GITHUB_TOKEN=${1-}               
+
+if [ -z "${GITHUB_TOKEN}" ]
+then 
+  echo 1>&2 GITHUB_TOKEN is undefined
+  exit 2
+fi
+
+
 if ! helm version &> /dev/null
 then
     echo "Helm not found, installing helm..."
@@ -9,12 +20,14 @@ then
     ./get_helm.sh
 fi
 
+# install cert-manager crds first
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.12.3/cert-manager.crds.yaml
 
 
-helm upgrade --install --create-namespace -f ./templates/github-runner-controller/values.yaml \
-    github-runner-controller ./templates/github-runner-controller --set github_token=${GITHUB_TOKEN} \
+# wait for github actions controller to be ready before lunching the runner deployment
+helm upgrade --install --create-namespace -f apps/github-runner-controller/values.yaml \
+    github-runner-controller apps/github-runner-controller --set github_token=${GITHUB_TOKEN} \
     --wait --namespace actions-runner-system --create-namespace
 
-helm upgrade --install --create-namespace -f ./templates/github-runner/values.yaml \
-    github-runner ./templates/github-runner --wait --namespace actions-runner-system 
+helm upgrade --install --create-namespace -f apps/github-runner/values.yaml \
+    github-runner apps/github-runner --wait --namespace actions-runner-system 
