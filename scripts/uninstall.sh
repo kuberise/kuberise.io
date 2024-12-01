a86fb50674bb2d839de5a9fd4cbf21b3c5d8bbd6
Mojtaba Imani <sm.imani@gmail.com>
2024-12-01 11:56:38 +0100

    Add additional namespaces to uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -41,7 +41,7 @@ then
   exit 2
 fi
 
-namespaces=("argocd" "cloudnative-pg" "keycloak" "backstage" "ingress-nginx-internal" "ingress-nginx-external" "monitoring" "ingresses" "gitea" "frontend-https" "metallb" "raw" "show-env" "cert-manager")
+namespaces=("argocd" "cloudnative-pg" "keycloak" "backstage" "ingress-nginx-internal" "ingress-nginx-external" "monitoring" "ingresses" "gitea" "frontend-https" "metallb" "raw" "show-env" "cert-manager" "external-dns" "internal-dns" "backend" "metrics-server")
 
 # Convert the array to a string with each namespace on a new line
 namespaces_str=$(IFS=$'\n'; echo "${namespaces[*]}")
