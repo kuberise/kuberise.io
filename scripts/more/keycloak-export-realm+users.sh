6c769615c64ae5122b2cd9c7c11e7ecf1ee2af5c
Mojtaba Imani <sm.imani@gmail.com>
2024-12-11 16:55:15 +0100

    Enable Keycloak in on-prem values and update export script for realm configuration

--- scripts/more/keycloak-export-realm+users.sh
+++ scripts/more/keycloak-export-realm+users.sh
@@ -2,4 +2,8 @@
 # This script exports the Keycloak realm configuration
 
 kcadm.sh config credentials --server  https://keycloak.onprem.kuberise.dev --realm master --user admin --password admin
-kc.sh export --realm testrealm --users same_file --file testrealm.json
+kc.sh export --realm platform --users realm_file --dir /Users/mojtaba/repo/github/kuberise/kuberise
+
+
+quay.io/keycloak/keycloak-operator:19.0.3-legacy
+quay.io/keycloak/keycloak-operator:26.0.7
