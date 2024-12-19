64446a76a7084c129cfaca5de0af12fe7d55d493
Mojtaba Imani <sm.imani@gmail.com>
2024-12-19 21:03:24 +0100

    Generates oauth2 client secret for argocd using external-secret (#100)

--- scripts/install.sh
+++ scripts/install.sh
@@ -332,6 +332,22 @@ if [ -n "${CLOUDFLARE_API_TOKEN}" ]; then
 fi
 
 
+# # Grafana OAuth2 Secrets
+# create_secret "$CONTEXT" "$NAMESPACE_MONITORING" "grafana-oauth2-client-secret" "--from-literal=client-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "grafana-oauth2-client-secret" "--from-literal=client-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# # PGAdmin OAuth2 Secrets
+# create_secret "$CONTEXT" "$NAMESPACE_PGADMIN" "keycloak-pgadmin-oauth2-client-secret" "--from-literal=CLIENT_SECRET=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "keycloak-pgadmin-oauth2-client-secret" "--from-literal=CLIENT_SECRET=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# # OAuth2-Proxy Secrets
+# create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "oauth2-proxy-client-secret" "--from-literal=client-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY --from-literal=client-id=oauth2-proxy --from-literal=cookie-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# # ArgoCD OAuth2 Secrets
+# create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "keycloak-argocd-oauth2-client-secret" "--from-literal=client-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY"
+# ARGOCD_CLIENT_SECRET=$(echo -n 'YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY' | base64)
+# kubectl patch secret argocd-secret -n $NAMESPACE_ARGOCD --patch "
+# data:
+#   oidc.keycloak.clientSecret: $ARGOCD_CLIENT_SECRET
+# "
+
 
 # Create secret for keycloak-operator to connect to Keycloak master realm.
 create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "keycloak-access" "--from-literal=username=admin --from-literal=password=$ADMIN_PASSWORD"
@@ -341,13 +357,6 @@ create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "keycloak-access" "--from-literal
 # Install ArgoCD with custom values and admin password
 VALUES_FILE="values/$PLATFORM_NAME/platform/argocd/values.yaml"
 install_argocd "$CONTEXT" "$NAMESPACE_ARGOCD" "$VALUES_FILE" "$ADMIN_PASSWORD" "$DOMAIN"
-# ArgoCD OAuth2 Secrets
-create_secret "$CONTEXT" "$NAMESPACE_KEYCLOAK" "keycloak-argocd-oauth2-client-secret" "--from-literal=client-secret=YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY" # FIXME: Should be generated randomly outside the script
-ARGOCD_CLIENT_SECRET=$(echo -n 'YqNdS8SBbI2iNPV0zs0LpUstTfy5iXKY' | base64) # FIXME: Should be generated randomly outside the script
-kubectl patch secret argocd-secret -n $NAMESPACE_ARGOCD --patch "
-data:
-  oidc.keycloak.clientSecret: $ARGOCD_CLIENT_SECRET
-"
 
 # Apply ArgoCD project and app of apps configuration
 deploy_app_of_apps "$CONTEXT" "$NAMESPACE_ARGOCD" "$PLATFORM_NAME" "$REPO_URL" "$TARGET_REVISION" "$DOMAIN"
